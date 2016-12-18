2b18b266765d58a2ed97154c0d2c10b93637fa38
BradleySharp <bradsharp0616@gmail.com>
2016-12-18 15:41:16 +0000

    Init Commit.

---
+++ Assets/Scripts/Earth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+public class Earth : MonoBehaviour {
+
+    public GameObject Planet;
+
+	// Use this for initialization
+	void Start () {
+        if (GetComponent<GameObject>())
+            Planet = GetComponent<GameObject>();
+        else
+            Debug.LogError("Char need GameObject.");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Planet.transform.localRotation *= Quaternion.AngleAxis(Time.deltaTime *100, Vector3.up);
+    }
+}
---
+++ Assets/Scripts/Orbit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    public float OrbitDegrees;
+    public float OrbitDegreesX;
+    public float OrbitDegreeZ;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = RotatePointAroundPivot(transform.position, transform.parent.position, Quaternion.Euler(OrbitDegreesX, OrbitDegrees * Time.deltaTime, OrbitDegreeZ));
+    }
+
+    public static Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Quaternion angle)
+    {
+        return angle * (point - pivot) + pivot;
+    }
+
+    /*  void FixedUpdate()
+      {
+          Vector3 diff = sphere.transomform.position - object.transform.position;
+          Vector3 direction = diff.normalized;
+          float gravitationalForce = (sphere.mass * object.mass * gravitationalConstant) / diff.sqrMagnitude;
+          object.rigidBody.addForce(direction * gravitationalForce);
+      }*/
+}
